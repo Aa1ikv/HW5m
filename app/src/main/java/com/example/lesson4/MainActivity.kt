972d3eb8ecf4273c6e55ebc59f9080e0903999be
Kulubekov Alisher <kulubekovalish@icloud.com>
2025-01-25 18:05:48 +0600

    Lesson

--- app/src/main/java/com/example/lesson4/MainActivity.kt
+++ app/src/main/java/com/example/lesson4/MainActivity.kt
@@ -1,36 +1,38 @@
 package com.example.lesson4
 
-import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.lesson4.data.CartoonAdapter
 import com.example.lesson4.databinding.ActivityMainBinding
+import com.example.lesson4.ui.adapter.CartoonAdapter
 
 class MainActivity : AppCompatActivity() {
+
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val viewModel: MainViewModel by viewModels()
-
+    private val adapter = CartoonAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        viewModel.getCharacters()
+
         setupRecyclerView()
 
-        viewModel.charactersLiveData.observe(this){
-            data -> //adapter.putList(data)
+        viewModel.charactersLiveData.observe(this) { characters ->
+            adapter.submitList(characters)
         }
-    }
-    private fun setupRecyclerView() = with(binding.rvCharacter){
-         layoutManager= LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
-        adapter = CartoonAdapter
+
+        viewModel.errorLiveData.observe(this) { error ->
+            Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+        }
+
+        viewModel.getCharacters()
     }
 
+    private fun setupRecyclerView() = with(binding.rvCharacter) {
+        layoutManager = LinearLayoutManager(this@MainActivity)
+        adapter = this@MainActivity.adapter
+    }
 }
